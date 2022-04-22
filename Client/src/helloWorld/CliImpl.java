05fff634c8fb0fca14733b0251aafbdc8e193809
horakhy <leonidas51000@hotmail.com>
2022-04-22 15:53:57 -0300

    a few changes

--- Client/src/helloWorld/CliImpl.java
+++ Client/src/helloWorld/CliImpl.java
@@ -2,10 +2,14 @@ package helloWorld;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.UUID;
 
 public class CliImpl extends UnicastRemoteObject implements InterfaceCli {
+  private int id;
+
   public CliImpl(InterfaceServ referenciaServidor) throws RemoteException {
-    referenciaServidor.registrarInteresse("Yoooo", this);
+    this.id = UUID.randomUUID().hashCode() % 100;
+    referenciaServidor.registrarInteresse(this.id + " Yoooo", this);
   }
 
   @Override
--- Server/MutualExclusionRMI/src/helloWorld/ServImpl.java
+++ Server/MutualExclusionRMI/src/helloWorld/ServImpl.java
@@ -2,18 +2,34 @@ package helloWorld;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.*;
 
 public class ServImpl extends UnicastRemoteObject implements InterfaceServ {
 
+  private int process;
+  private int token = 0;
+  private ArrayList<InterfaceCli> clientesEmEspera1;
+  private ArrayList<InterfaceCli> clientesEmEspera2;
+
   public ServImpl() throws RemoteException {
     super();
+    this.clientesEmEspera1 = new ArrayList<InterfaceCli>();
+    this.clientesEmEspera2 = new ArrayList<InterfaceCli>();
   }
 
   @Override
   public void registrarInteresse(String text, InterfaceCli referenciaCliente) throws RemoteException {
-    referenciaCliente.notificar(text);
+    if (clientesEmEspera1.size() <= 0) {
+      referenciaCliente.notificar(text);
+      return;
+    }
+      clientesEmEspera1.add(referenciaCliente);
+      referenciaCliente.notificar("O recurso está sendo utilizado por outro cliente");
     
   }
 
-}
+  public void enviarResposta(String text, InterfaceCli referenciaCliente) throws RemoteException {
+    referenciaCliente.notificar(text);
+  }
 
+}
